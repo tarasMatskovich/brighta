d12d45710d2eec1375433f761d0b5dde7bcb755a
tarasMatskovich <matskovich.taras@gmail.com>
2019-04-10 15:22:27 +0300

    Made Router class and Base Controller class

--- index.php
+++ index.php
@@ -2,3 +2,10 @@
 
 define("ROOT", dirname(__FILE__));
 
+
+require_once(ROOT . '/components/autoload.php');
+
+require_once(ROOT . '/components/Router.php');
+
+$router = new App\Components\Router();
+$router->handle();
